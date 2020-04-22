943b24aa4fdfa7b4f1d5036426a497363b9288e6
toha <senor.toha@gmail.com>
2020-04-22 13:11:04 +0600

    hola

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ print("hello world")
 print("hello develop")
 import json
 
-x = None
+x = 'lol'
 
 x = {'commits': [{'id': '08c926a8cf657c995463c523268ccb1422654edd', 'tree_id': '0a5db9a27874e9c946f6e007e69287780c633081', 
      'distinct': True, 'message': "'first'", 'timestamp': '2020-04-22T11:24:41+06:00', 'url': 'https://github.com/Toha-K-M/test/commit/08c926a8cf657c995463c523268ccb1422654edd', 
@@ -16,3 +16,4 @@ print(type(x), x['commits'])
 for item in x['commits']:
 	for obj in item:
 		print(obj, " : ", item[obj])
+		if obj 
